974c3877e227779319f0228a47054b19d1273bd4
Abhishek kumar <abhishek.kumar@daffodilsw.com>
2024-10-19 22:05:28 +0530

    updates

--- Excel/Spreadsheet.js
+++ Excel/Spreadsheet.js
@@ -1,20 +1,35 @@
-const Cell = require("./Cell")
+const Cell = require("./Cell");
 
 class Spreadsheet {
-    constructor(row, col) {
-        this.row = row;
-        this.col = col;
-        this.cells = new Array(this.row).fill(new Array(this.col).fill(new Cell()));
-    }
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+    this.cells = Array.from({ length: this.row }).map(() =>
+      Array.from({ length: this.col }).map(() => new Cell())
+    );
+  }
 
-    getCell(row, col) {
-        return this.cells[row][col];
-    }
+  getCell(row, col) {
+    return this.cells[row][col];
+  }
 
-    getCells() {
-        return this.cells
-    }
+  getCells() {
+    return this.cells;
+  }
 
+  addRow(value) {
+    this.row++;
+    this.cells.push(
+      Array.from({ length: this.col }).map(() => new Cell(value))
+    );
+  }
+
+  addCol(value) {
+    this.col++;
+    this.cells.forEach((doc) => {
+      doc.push(new Cell(value));
+    });
+  }
 }
 
-module.exports = Spreadsheet
+module.exports = Spreadsheet;
--- Excel/index.js
+++ Excel/index.js
@@ -1,11 +1,4 @@
 const Spreadsheet = require("./Spreadsheet");
-
 const worksheet1 = new Spreadsheet(5, 5);
-
-console.log(worksheet1.cells);
-
-// for(let i= 0; i < 5; i++) {
-    console.log(worksheet1.getCell(0,1).editValue(5))
-// }
 
 console.log(worksheet1.cells);
